84194ec9cb65a98051463816230cc79a5aaebf8b
weee <lujainkaroush2@gmail.com>
2026-08-20 15:59:46 +0300

    fix upload images

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    ->booted(function () {
+        app()->usePublicPath(base_path('../public_html'));
+    })
     ->withMiddleware(function (Middleware $middleware): void {
 
         $middleware->alias([
