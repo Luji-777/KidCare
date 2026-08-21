564dc2b9bafa52aeb7ef798e8de45fabe7958ecc
weee <lujainkaroush2@gmail.com>
2026-08-21 16:02:15 +0300

    title notifications

--- database/migrations/2026_04_26_170322_create_notifications_table.php
+++ database/migrations/2026_04_26_170322_create_notifications_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->foreignId('parent_id')->constrained('parent_models')->cascadeOnDelete();
+            $table->string('title');
             $table->text('message');
             $table->timestamps();
         });
